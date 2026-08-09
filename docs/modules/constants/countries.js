128fc922d2900ff824e54c75bf23c4d9a6e6ad4f
burczynskikamil <kamilpiotrburczynski@gmail.com>
2026-08-09 09:53:28 +0200

    Add countries constants module

---
+++ docs/modules/constants/countries.js
@@ -0,0 +1,15 @@
+// modules/constants/countries.js
+window.COUNTRIES = [
+  {code:'PL',name:'Polska',flag:'https://flagsapi.com/PL/flat/64.png',color:'#ff4d4f'},
+  {code:'NL',name:'Holandia',flag:'https://flagsapi.com/NL/flat/64.png',color:'#ff7a18'},
+  {code:'GB',name:'Anglia',flag:'https://flagsapi.com/GB/flat/64.png',color:'#3f7fff'},
+  {code:'ES',name:'Hiszpania',flag:'https://flagsapi.com/ES/flat/64.png',color:'#ffb300'},
+  {code:'DE',name:'Niemcy',flag:'https://flagsapi.com/DE/flat/64.png',color:'#222831'},
+  {code:'PT',name:'Portugalia',flag:'https://flagsapi.com/PT/flat/64.png',color:'#2db34a'},
+  {code:'IT',name:'Włochy',flag:'https://flagsapi.com/IT/flat/64.png',color:'#009246'},
+  {code:'FR',name:'Francja',flag:'https://flagsapi.com/FR/flat/64.png',color:'#002395'},
+  {code:'AR',name:'Argentyna',flag:'https://flagsapi.com/AR/flat/64.png',color:'#4ea8de'},
+  {code:'BR',name:'Brazylia',flag:'https://flagsapi.com/BR/flat/64.png',color:'#1fbf4a'},
+  {code:'CN',name:'Chiny',flag:'https://flagsapi.com/CN/flat/64.png',color:'#e63946'},
+  {code:'ZA',name:'RPA',flag:'https://flagsapi.com/ZA/flat/64.png',color:'#f77f00'},
+];
